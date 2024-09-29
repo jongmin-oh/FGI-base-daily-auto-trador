94f4f47b2edd19168200793c4e1bd77022ad85b6
jongmin-oh <alswhddh@naver.com>
2024-09-29 19:46:19 +0900

    [FEAT] docker image Lambda SAM deploy

--- README.md
+++ README.md
@@ -1,4 +1,2 @@
 # FGI-daily-trader
 FGI-based daily trader
-
-0 23 * * * /home/acdong/jmoh/stock-auto-trade/run.sh
--- run.sh
+++ run.sh
@@ -1,1 +1,14 @@
-python3 /home/acdong/jmoh/stock-auto-trade/trading.py
+#!/bin/bash
+AWS_PROFILE=acdong
+STACK_NAME=FGI-daily-trader
+
+# 빌드
+sam build --profile $AWS_PROFILE
+
+# 로컬 실행
+# sam local invoke FGIDailyTraderFunction --profile $AWS_PROFILE
+
+# 배포
+sam deploy
+
+sam logs -n LambdaFunction --stack-name $STACK_NAME --tail --profile $AWS_PROFILE
